84f6f54e00afea7000c173265920e796b8099a3b
Leonardo Falconi <leonardofalconi@Leonardos-Mac-mini.local>
2024-10-25 14:27:13 -0300

    chore: create global styles and improve theme variables

--- src/components/AppProviders/index.tsx
+++ src/components/AppProviders/index.tsx
@@ -1,7 +1,11 @@
 import { ThemeProvider } from 'styled-components'
 import { Theme } from '../../theme'
 import { FC, ReactNode } from 'react'
+import GlobalStyles from '../../theme/GlobalStyles'
 
 export const AppProviders: FC<{ children: ReactNode }> = ({ children }) => (
-  <ThemeProvider theme={Theme}>{children}</ThemeProvider>
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+    {children}
+  </ThemeProvider>
 )
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,13 +1,47 @@
 export const Theme = {
-  colors: {},
-  fontFamily: 'Montserrat, sans-serif',
+  colors: {
+    grape: '#6324C6',
+    white: '#FFFFFF',
+    selectiveYellow: '#FFB800',
+  },
   typography: {
-    fontWeight: {
-      bold: 700,
-      semiBold: 600,
-      medium: 500,
-      regular: 400,
+    fontMulish: {
+      family: '"Mulish", sans-serif',
+      weight: {
+        black: 900,
+        extraBold: 800,
+        bold: 700,
+        semiBold: 600,
+        medium: 500,
+        regular: 400,
+        light: 300,
+        extraLight: 200,
+      },
     },
   },
-  media: {},
+  media: {
+    mobile: {
+      size: '10px',
+    },
+    tablet: {
+      query: '@media (min-width: 768px)',
+      size: '15px',
+    },
+    HD: {
+      query: '@media (min-width: 1280px)',
+      size: '20px',
+    },
+    fullHD: {
+      query: '@media (min-width: 1920px)',
+      size: '30px',
+    },
+    '2k': {
+      query: '@media (min-width: 2560px)',
+      size: '40px',
+    },
+    '4k': {
+      query: '@media (min-width: 3840px)',
+      size: '60px',
+    },
+  },
 }
