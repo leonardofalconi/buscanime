b084dbc7e08776a31ca9cc980a44e844e00cf6d4
Leonardo Falconi <leonardofalconi@Leonardos-Mac-mini.local>
2024-10-29 14:42:35 -0300

    chore: add apollo provider and configure

--- src/components/AppProviders/index.tsx
+++ src/components/AppProviders/index.tsx
@@ -2,10 +2,12 @@ import { ThemeProvider } from 'styled-components'
 import { Theme } from '../../theme'
 import { FC, ReactNode } from 'react'
 import GlobalStyles from '../../theme/GlobalStyles'
+import { ApolloProvider } from '@apollo/client'
+import { apolloClient } from '../../clients/Apollo'
 
 export const AppProviders: FC<{ children: ReactNode }> = ({ children }) => (
   <ThemeProvider theme={Theme}>
     <GlobalStyles />
-    {children}
+    <ApolloProvider client={apolloClient}>{children}</ApolloProvider>
   </ThemeProvider>
 )
